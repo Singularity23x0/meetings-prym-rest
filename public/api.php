d2656d017a93de86222750136e4ed953b4dc98a3
pastuszk <pastuszk@d17-l428-pc-04.ipa.ki.agh.edu.pl>
2020-01-14 09:27:29 +0100

    SQL added

--- public/api.php
+++ public/api.php
@@ -18,12 +18,30 @@ $app->get(
 $app->get(
     '/api/participants',
     function (Request $request, Response $response, array $args) {
-        $participants = [
-            ['id' => 1, 'firstname' => 'John', 'lastname' => 'Doe'],
-            ['id' => 2, 'firstname' => 'Kate', 'lastname' => 'Pig'],
-            ['id' => 3, 'firstname' => 'Chris', 'lastname' => 'Lualigi'],
-            ['id' => 4, 'firstname' => 'Author', 'lastname' => 'McFakename']
-        ];
+
+        class MyDB extends SQLite3 {
+            function __construct() {
+                $this->open('../participants.db');
+            }
+        }
+        $db = new MyDB();
+        if(!$db) {
+            echo $db->lastErrorMsg();
+            exit();
+        }
+
+        $participants = [];
+
+        $sql = "SELECT id, firstname, lastname FROM participant";
+        $ret = $db->query($sql);
+        while($row = $ret->fetchArray(SQLITE3_ASSOC)){
+            $participants[] = [
+               'id' => $row['id'],
+                'firstname' => $row['firstname'],
+                'lastname' => $row['lastname']
+            ];
+        }
+
         return $response->withJson($participants);
     }
 );
